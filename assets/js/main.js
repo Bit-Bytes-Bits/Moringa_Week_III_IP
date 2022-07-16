10dd4ef7e5c1d97e71a41ce0b6210118bcdf0e4f
Bit-Bytes-Bits <ian.kaibi@student.moringaschool.com>
2022-07-16 11:11:21 +0300

    layout the structure of the js file with appropriate comments

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+// buying price is ksh 45
+
+var sellingPrice = 45;
+// Writing a function that gives you a report on how much milk is produced per shed and the total amount of milk produced by all sheds.
+
+function totalProdution(params) {
+    
+}
+
+// Writing a function that takes in the Brookside buying rate as one of its arguments, gives you a report on how much pay you'll receive over time. For instance, it can give a report on how much you will earn on a weekly basis or even on a yearly basis. For example;
+
+function incomeOverTime(sellingPrice, time) {
+
+}
+
+// Create a report on how much you'll make each month in a leap year. (Hint: Consider using objects.)
+
+
+
+
+
+
+
+
+// Brookside Dairies decided to raise the rate at which they buy a litre from Ksh.45 to Ksh.49.60. Write a function that gives you a report on the comparison of how much you'll make every month and the difference from the previous rate.
