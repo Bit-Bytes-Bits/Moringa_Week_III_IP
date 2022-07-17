fc59148397b6f40f48073818588f465d8cc58659
Bit-Bytes-Bits <ian.kaibi@student.moringaschool.com>
2022-07-18 02:14:54 +0300

    modified total production function

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,5 @@
+//overlay the second div
+
 var nextbtn = document.querySelector('.next-btn');
 var popupformbg =  document.querySelector('.popup-form-bg');
 var closeform = document.querySelector('.close-form')
@@ -10,7 +12,41 @@ closeform.addEventListener('click', function(){
     popupformbg.classList.remove('popup-form-bg-active');
 })
 
-console.log(34);
+// variables
+
+// var firstShedd = parseInt(document.getElementById('shedA').value);
+// console.log(firstShedd);
+// var secondShed = parseInt(document.getElementById(shedB).value);
+let a = parseInt(document.getElementById('shedA').value);
+let b = parseInt(document.getElementById('shedB').value);
+let c = parseInt(document.getElementById('shedC').value);
+let d = parseInt(document.getElementById('shedD').value);
+
+document.getElementById("dailysubmit").addEventListener("click", function() {
+    totalProduction(a,b,c,d);
+  });
+
+  selling_price = 45;
+  time = [7, 366];
+
+function totalProduction(a , b, c, d){
+   
+    dailyTotal = (a+b+c+d);
+    document.getElementById("calculateDaily").value = "sasa " + dailyTotal;
+    // console.log(a+b);
+    document.getElementById("calculateWeekly").value = "sasa " + (selling_price * dailyTotal*time[0]);
+
+    document.getElementById("calculateYearly").value = "sasa " + (selling_price * dailyTotal*time[1]);
+}
+// console.log
+
+// function weeklyIncome(selling_price, time);
+// = document.getElementById('.weekly-submit');
+// weeklyIncome.addEventListener('click', totalProduction);{
+    
+// };
+
+
 // // buying price is ksh 45
 
 // let sellingPrice = 45;
@@ -18,32 +54,32 @@ console.log(34);
 
 // // Writing a function that gives you a report on how much milk is produced per shed and the total amount of milk produced by all sheds.
 
-let sheds = {
-    'A': 510,
-    'B': 308,
-    'C': 486,
-    'D': 572
-}
+// let sheds = {
+//     'A': 510,
+//     'B': 308,
+//     'C': 486,
+//     'D': 572
+// }
 
 // var myArray = new Array(Object.keys(sheds))
-var keys = Object.keys(sheds);
+// var keys = Object.keys(sheds);
 // console.log(myArray);
 
 // for()
 
 // total production in a day
 
-function totalProdution({A,B,C,D}) {
+// function totalProdution({A,B,C,D}) {
     
-    let totalProdution = (A + B + C + D);
-    return totalProdution;
-}
-console.log(totalProdution(sheds));
+//     let totalProdution = (A + B + C + D);
+//     return totalProdution;
+// }
+// console.log(totalProdution(sheds));
 
 // console.log("Production for shed " + keys[0] + " is " + sheds.A + " liters today.");
 
-const prod = document.getElementById("shed-a");
-prod.textContent="sasa";
+// const prod = document.getElementById("shed-a");
+// prod.textContent="sasa";
 // prod.textContent ="Production for shed " + keys[0] + " is " + sheds.A + " liters today.<br> Production for shed " + keys[1] + " is " + sheds.B + " liters today.";
 
 
