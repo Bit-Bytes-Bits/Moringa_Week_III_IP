d93172f0ef3da649831a3da6b4c4f465b450c9ec
Bit-Bytes-Bits <ian.kaibi@student.moringaschool.com>
2022-07-17 10:36:18 +0300

    fixing the production function to display on html

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 // buying price is ksh 45
 
 let sellingPrice = 45;
-
+// var time;
 
 // Writing a function that gives you a report on how much milk is produced per shed and the total amount of milk produced by all sheds.
 
@@ -11,19 +11,51 @@ let sheds = {
     'C': 486,
     'D': 572
 }
+
+var myArray = new Array(Object.keys(sheds))
+var keys = Object.keys(sheds);
+console.log(myArray);
+
+// for()
+
+// total production in a day
+
 function totalProdution({A,B,C,D}) {
-    let totalProdution = (A + B + C + D)
-    return totalProdution
+    
+    let totalProdution = (A + B + C + D);
+    return totalProdution;
 }
 console.log(totalProdution(sheds));
 
-// console.log(sheds);
+// console.log("Production for shed " + keys[0] + " is " + sheds.A + " liters today.");
+
+const prod = document.getElementById("production");
+prod.innerHTML ="Production for shed " + keys[0] + " is " + sheds.A + " liters today.";
+
+
 
 // Writing a function that takes in the Brookside buying rate as one of its arguments, gives you a report on how much pay you'll receive over time. For instance, it can give a report on how much you will earn on a weekly basis or even on a yearly basis. For example;
 
-function incomeOverTime(sellingPrice, time) {
 
+var time = [7, 366]
+console.log(time[1]* 2);
+function incomeOverTime(sellingPrice, time) {
+    
+    function totalProdution({A,B,C,D}) {
+    
+        let totalProdution = (A + B + C + D);
+        return totalProdution;
+    }
+
+    weeklyIncome = totalProdution * time[0];
+
+    yearlyIncome = totalProdution * time[1];
+
+    return weeklyIncome;
+    
 }
+
+console.log(weeklyIncome);
 
 // Create a report on how much you'll make each month in a leap year. (Hint: Consider using objects.)
 
