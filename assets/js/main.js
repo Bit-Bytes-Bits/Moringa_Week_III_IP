2fbbbd186ccf97fb9e810a662a81a077cd657471
Bit-Bytes-Bits <ian.kaibi@student.moringaschool.com>
2022-07-18 03:54:33 +0300

    adding text boxes for price comparison

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,78 +1,165 @@
 //overlay the second div
 
-var nextbtn = document.querySelector('.next-btn');
-var popupformbg =  document.querySelector('.popup-form-bg');
-var closeform = document.querySelector('.close-form')
+var nextbtn = document.querySelector(".next-btn");
+var popupformbg = document.querySelector(".popup-form-bg");
+var closeform = document.querySelector(".close-form");
 
-nextbtn.addEventListener('click', function(){
-    popupformbg.classList.add('popup-form-bg-active');
+nextbtn.addEventListener("click", function () {
+  popupformbg.classList.add("popup-form-bg-active");
 });
 
-closeform.addEventListener('click', function(){
-    popupformbg.classList.remove('popup-form-bg-active');
-})
+closeform.addEventListener("click", function () {
+  popupformbg.classList.remove("popup-form-bg-active");
+});
 
-let a = parseInt(document.getElementById('shedA').value);
-let b = parseInt(document.getElementById('shedB').value);
-let c = parseInt(document.getElementById('shedC').value);
-let d = parseInt(document.getElementById('shedD').value);
+let a = parseInt(document.getElementById("shedA").value);
+let b = parseInt(document.getElementById("shedB").value);
+let c = parseInt(document.getElementById("shedC").value);
+let d = parseInt(document.getElementById("shedD").value);
 
-document.getElementById("dailysubmit").addEventListener("click", function() {
-    totalProduction(a,b,c,d);
-  });
+document.getElementById("dailysubmit").addEventListener("click", function () {
+  totalProduction(a, b, c, d);
+});
 
-  selling_price = 45;
-  time = [7, 366];
+selling_price = 45;
+time = [7, 366];
 
-function totalProduction(a , b, c, d){
-   
-    dailyMilkProduction = (a+b+c+d);
-    dailyTotal = dailyMilkProduction * selling_price;
-    document.getElementById("calculateDaily").value = "The total production is " + dailyMilkProduction + " liters per day.";
-    // console.log(a+b);
-    document.getElementById("calculateWeekly").value = "Your weekly income will be Ksh " + (selling_price * dailyMilkProduction*time[0]);
+function totalProduction(a, b, c, d) {
+  dailyMilkProduction = a + b + c + d;
+  dailyTotal = dailyMilkProduction * selling_price;
+  document.getElementById("calculateDaily").value =
+    "The total production is " + dailyMilkProduction + " liters per day.";
+  // console.log(a+b);
+  document.getElementById("calculateWeekly").value =
+    "Your weekly income will be Ksh " +
+    selling_price * dailyMilkProduction * time[0];
 
-    document.getElementById("calculateYearly").value = "Your yearly income will be Ksh " + (selling_price * dailyMilkProduction*time[1]);
+  document.getElementById("calculateYearly").value =
+    "Your yearly income will be Ksh " +
+    selling_price * dailyMilkProduction * time[1];
 
-    incomeJanuary = dailyTotal * 31;
-    document.getElementById("january").value = "Your income for January is " + (incomeJanuary);
-    incomeFebruary = dailyTotal * 29;
-    document.getElementById("february").value = "Your income for February is " + (incomeFebruary);
-    incomeMarch = dailyTotal * 31;
-    document.getElementById("march").value = "Your income for March is " + (incomeMarch);
-    incomeApril = dailyTotal * 30;
-    document.getElementById("april").value = "Your income for April is " + (incomeApril);
-    incomeMay = dailyTotal * 31;
-    document.getElementById("may").value = "Your income for May is " + (incomeMay);
-    incomeJune = dailyTotal * 30;
-    document.getElementById("june").value = "Your income for June is " + (incomeJune);
-    incomeJuly = dailyTotal * 31;
-    document.getElementById("july").value = "Your income for July is " + (incomeJuly);
-    incomeAugust = dailyTotal * 31;
-    document.getElementById("august").value = "Your income for August is " + (incomeAugust);
-    incomeSeptember = dailyTotal * 30;
-    document.getElementById("september").value = "Your income for September is " + (incomeSeptember);
-    incomeOctober = dailyTotal * 31;
-    document.getElementById("october").value = "Your income for October is " + (incomeOctober);
-    incomeNovember = dailyTotal * 30;
-    document.getElementById("november").value = "Your income for November is " + (incomeNovember);
-    incomeDecember = dailyTotal * 31;
-    document.getElementById("december").value = "Your income for December is " + (incomeDecember);
+  incomeJanuary = dailyTotal * 31;
+  document.getElementById("january").value =
+    "Your income for January is " + incomeJanuary;
+  incomeFebruary = dailyTotal * 29;
+  document.getElementById("february").value =
+    "Your income for February is " + incomeFebruary;
+  incomeMarch = dailyTotal * 31;
+  document.getElementById("march").value =
+    "Your income for March is " + incomeMarch;
+  incomeApril = dailyTotal * 30;
+  document.getElementById("april").value =
+    "Your income for April is " + incomeApril;
+  incomeMay = dailyTotal * 31;
+  document.getElementById("may").value = "Your income for May is " + incomeMay;
+  incomeJune = dailyTotal * 30;
+  document.getElementById("june").value =
+    "Your income for June is " + incomeJune;
+  incomeJuly = dailyTotal * 31;
+  document.getElementById("july").value =
+    "Your income for July is " + incomeJuly;
+  incomeAugust = dailyTotal * 31;
+  document.getElementById("august").value =
+    "Your income for August is " + incomeAugust;
+  incomeSeptember = dailyTotal * 30;
+  document.getElementById("september").value =
+    "Your income for September is " + incomeSeptember;
+  incomeOctober = dailyTotal * 31;
+  document.getElementById("october").value =
+    "Your income for October is " + incomeOctober;
+  incomeNovember = dailyTotal * 30;
+  document.getElementById("november").value =
+    "Your income for November is " + incomeNovember;
+  incomeDecember = dailyTotal * 31;
+  document.getElementById("december").value =
+    "Your income for December is " + incomeDecember;
 
+  // ---------------- price comparison
+  newSellingPrice = 49.6;
+  newDailyTotal = dailyMilkProduction * newSellingPrice;
 
-    
+  newIncomeJanuary = newDailyTotal * 31;
+  document.getElementById("newPrice-january").value =
+    "Your income for January is " + newIncomeJanuary;
+  newIncomeFebruary = newDailyTotal * 29;
+  document.getElementById("newPrice-february").value =
+    "Your income for February is " + newIncomeFebruary;
+  newIncomeMarch = newDailyTotal * 31;
+  document.getElementById("newPrice-march").value =
+    "Your income for March is " + newIncomeMarch;
+  newIncomeApril = newDailyTotal * 30;
+  document.getElementById("newPrice-april").value =
+    "Your income for April is " + newIncomeApril;
+  newIncomeMay = newDailyTotal * 31;
+  document.getElementById("newPrice-may").value =
+    "Your income for May is " + newIncomeMay;
+  newIncomeJune = newDailyTotal * 30;
+  document.getElementById("newPrice-june").value =
+    "Your income for June is " + newIncomeJune;
+  newIncomeJuly = newDailyTotal * 31;
+  document.getElementById("newPrice-july").value =
+    "Your income for July is " + newIncomeJuly;
+  newIncomeAugust = newDailyTotal * 31;
+  document.getElementById("newPrice-august").value =
+    "Your income for August is " + newIncomeAugust;
+  newIncomeSeptember = newDailyTotal * 30;
+  document.getElementById("newPrice-september").value =
+    "Your income for September is " + newIncomeSeptember;
+  newIncomeOctober = newDailyTotal * 31;
+  document.getElementById("newPrice-october").value =
+    "Your income for October is " + newIncomeOctober;
+  newIncomeNovember = newDailyTotal * 30;
+  document.getElementById("newPrice-november").value =
+    "Your income for November is " + newIncomeNovember;
+  newIncomeDecember = newDailyTotal * 31;
+  document.getElementById("newPrice-december").value =
+    "Your income for December is " + newIncomeDecember;
 
-
-
+  // ---------- difference in price
+  janDifference = newIncomeJanuary - incomeJanuary;
+  document.getElementById("janDifference").value =
+    "The difference is ksh " + Math.round(janDifference) + " more.";
+  febDifference = newIncomeFebruary - incomeFebruary;
+  document.getElementById("febDifference").value =
+    "The difference is ksh " + Math.round(febDifference) + " more.";
+  marDifference = newIncomeMarch - incomeMarch;
+  document.getElementById("marDifference").value =
+    "The difference is ksh " + Math.round(marDifference) + " more.";
+  aprDifference = newIncomeApril - incomeApril;
+  document.getElementById("aprDifference").value =
+    "The difference is ksh " + Math.round(aprDifference) + " more.";
+  mayDifference = newIncomeMay - incomeMay;
+  document.getElementById("mayDifference").value =
+    "The difference is ksh " + Math.round(mayDifference) + " more.";
+  junDifference = newIncomeJune - incomeJune;
+  document.getElementById("junDifference").value =
+    "The difference is ksh " + Math.round(junDifference) + " more.";
+  julDifference = newIncomeJuly - incomeJuly;
+  document.getElementById("julDifference").value =
+    "The difference is ksh " + Math.round(julDifference) + " more.";
+  augDifference = newIncomeAugust - incomeAugust;
+  document.getElementById("augDifference").value =
+    "The difference is ksh " + Math.round(augDifference) + " more.";
+  sepDifference = newIncomeSeptember - incomeSeptember;
+  document.getElementById("sepDifference").value =
+    "The difference is ksh " + Math.round(sepDifference) + " more.";
+  octDifference = newIncomeOctober - incomeOctober;
+  document.getElementById("octDifference").value =
+    "The difference is ksh " + Math.round(octDifference) + " more.";
+  novDifference = newIncomeNovember - incomeNovember;
+  document.getElementById("novDifference").value =
+    "The difference is ksh " + Math.round(novDifference) + " more.";
+  decDifference = newIncomeDecember - incomeDecember;
+  document.getElementById("decDifference").value =
+    "The difference is ksh " + Math.round(decDifference) + " more.";
 }
 // console.log
 
 // function weeklyIncome(selling_price, time);
 // = document.getElementById('.weekly-submit');
 // weeklyIncome.addEventListener('click', totalProduction);{
-    
-// };
 
+// };
 
 // // buying price is ksh 45
 
@@ -97,7 +184,7 @@ function totalProduction(a , b, c, d){
 // total production in a day
 
 // function totalProdution({A,B,C,D}) {
-    
+
 //     let totalProdution = (A + B + C + D);
 //     return totalProdution;
 // }
@@ -109,18 +196,14 @@ function totalProduction(a , b, c, d){
 // prod.textContent="sasa";
 // prod.textContent ="Production for shed " + keys[0] + " is " + sheds.A + " liters today.<br> Production for shed " + keys[1] + " is " + sheds.B + " liters today.";
 
-
-
-
 // // Writing a function that takes in the Brookside buying rate as one of its arguments, gives you a report on how much pay you'll receive over time. For instance, it can give a report on how much you will earn on a weekly basis or even on a yearly basis. For example;
-
 
 // var time = [7, 366]
 // console.log(time[1]* 2);
 // function incomeOverTime(sellingPrice, time) {
-    
+
 //     function totalProdution({A,B,C,D}) {
-    
+
 //         let totalProdution = (A + B + C + D);
 //         return totalProdution;
 //     }
@@ -130,18 +213,11 @@ function totalProduction(a , b, c, d){
 //     yearlyIncome = totalProdution * time[1];
 
 //     return weeklyIncome;
-    
+
 // }
 
 // console.log(weeklyIncome);
 
 // // Create a report on how much you'll make each month in a leap year. (Hint: Consider using objects.)
-
-
-
-
-
-
-
 
 // // Brookside Dairies decided to raise the rate at which they buy a litre from Ksh.45 to Ksh.49.60. Write a function that gives you a report on the comparison of how much you'll make every month and the difference from the previous rate.
